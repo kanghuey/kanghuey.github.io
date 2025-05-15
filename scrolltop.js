8ba0ee2961cb450c73ab813ced5a03713c9dff7d
Cheng Kang Huey <148413394+kanghuey@users.noreply.github.com>
2025-05-15 10:06:01 +0800

    Update scrolltop.js

--- scrolltop.js
+++ scrolltop.js
@@ -1,13 +1,2 @@
 //Cheng Kang Huey A23CS0216 Section 10
-
-document.addEventListener("DOMContentLoaded", function () {
-    const topBtn = document.getElementById("topBtn");
-
-    window.addEventListener("scroll", () => {
-        topBtn.style.display = window.scrollY > 200 ? "block" : "none";
-    });
-
-    topBtn.addEventListener("click", () => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-    });
-});
+document.addEventListener("DOMContentLoaded",function(){let e=document.getElementById("topBtn");window.addEventListener("scroll",()=>{e.style.display=window.scrollY>200?"block":"none"}),e.addEventListener("click",()=>{window.scrollTo({top:0,behavior:"smooth"})})});
